bc3c0be89ab8a5f6b5f140ecb9cef42aac378ce4
Esben <esbennj@hotmail.com>
2020-04-18 01:00:17 +0200

    Audio: buzz pitch y-axis

--- Assets/Scripts/Audio/FlyBuzz.cs
+++ Assets/Scripts/Audio/FlyBuzz.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class FlyBuzz : MonoBehaviour
 {
+    AudioSource audioSource;
+    Vector3 lerpin;
+    Vector3 vel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        vel = FlyController2.flyVelocity;
+
+        audioSource.pitch = 1 + lerpin.y;
         
+        lerpin = Vector3.Lerp(lerpin, vel, 0.001f);
+        Debug.Log("Lerp: " + lerpin.y);
+//        Debug.Log("speed: " + FlyController2.speed);
+        Debug.Log("velx: " + vel.x);
+        Debug.Log("vely: " + vel.y);
+        Debug.Log("velz: " + vel.z);
     }
 }
