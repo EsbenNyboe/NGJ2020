ecde91ffa8eb9dde1ad0915cbbed1898af37c3f3
Victor <vickim_@hotmail.com>
2020-04-19 01:07:54 +0200

    collision sound primary and secondary sounds

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -7,6 +7,8 @@ public class SoundManager : MonoBehaviour
     public AudioEvent[] collisionVoices;
     public int numCollVoices;
     public int colIndex =-1;
+    public bool firstPlay = true;
+    public float cooldownTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,13 +24,46 @@ public class SoundManager : MonoBehaviour
         if (colIndex == numCollVoices - 1)
             collisionVoices[0].StopSound();
         else if(collisionVoices[colIndex+1] != null)
-            collisionVoices[colIndex + 1].StopSound(); 
-        audioEvent.PlaySound();
+            collisionVoices[colIndex + 1].StopSound();
+
+        if (firstPlay)
+        {
+            audioEvent.PlaySound(0);
+            StartCoroutine(CoolDownCoroutine());
+        }
+        else
+        {
+            audioEvent.PlaySound(1);
+        }
+                
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    bool checkIfNoOneIsPlaying()
+    {
+        bool noOnePlaying = true; 
+        foreach (AudioEvent a in collisionVoices)
+        {
+            if (a != null)
+                if (a.isPlaying())
+                {
+                    noOnePlaying = false;
+                    break;
+                }
+        }
+        return noOnePlaying;
+    }
+    IEnumerator CoolDownCoroutine()
+    {
+        firstPlay = false;
+        float scale = Time.timeScale;
+        yield return new WaitForSeconds(cooldownTime * scale);
+        firstPlay = true;
     }
 }
